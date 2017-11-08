d7c240c2b8a8dc5263cba1426b8a664d35fe04bd
levioni <33033952+levioni@users.noreply.github.com>
2017-11-08 20:10:03 +0200

    Add files via upload

---
+++ WriteToCSV.java
@@ -0,0 +1,183 @@
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.swing.plaf.synth.SynthSpinnerUI;
+
+import java.util.ArrayList;
+import java.io.FileWriter;
+
+public class WriteToCSV {
+	/**
+	 * source of the code1: https://www.mkyong.com/java/how-to-read-and-parse-csv-file-in-java/
+	 * source of the code2: https://www.mkyong.com/java/how-to-export-data-to-csv-file-java/
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/**
+		 * this section input a WigleWifi csv file and make it csv file accoring
+		 * to what we need to do in the assignment (file name of the input is "test4")
+		 * 
+		 * please note that you need to change the directory location according to
+		 * your computer for it to read and export the file successfully
+		 */
+
+		String csvfile="C:\\Users\\computer\\Desktop\\csv\\test4.csv";
+		BufferedReader br = null;
+		String line = "";
+		String cvsSplitBy = ",";
+
+		try {
+
+			boolean skipfirst=false;
+			br = new BufferedReader(new FileReader(csvfile));
+			List<ArrayList<String>> inputCSV=new ArrayList<ArrayList<String>>();
+			ArrayList<String> headlines=new ArrayList<String>();
+			while ((line = br.readLine()) != null)
+			{
+				// use comma as separator
+				String[] Getline = line.split(cvsSplitBy);
+				if(!skipfirst)
+				{
+					skipfirst=true;
+					headlines.add(Getline[0]);
+					headlines.add(Getline[1]);
+					headlines.add(Getline[2]);
+					headlines.add(Getline[3]);
+					headlines.add(Getline[4]);
+					headlines.add(Getline[5]);
+					headlines.add(Getline[6]);
+					headlines.add(Getline[7]);
+					headlines.add(Getline[8]);
+					headlines.add(Getline[9]);
+					headlines.add(Getline[10]);
+				}
+				else
+				{
+					ArrayList<String> inner=new ArrayList<String>();
+					inner.add(Getline[0]);
+					inner.add(Getline[1]);
+					inner.add(Getline[2]);
+					inner.add(Getline[3]);
+					inner.add(Getline[4]);
+					inner.add(Getline[5]);
+					inner.add(Getline[6]);
+					inner.add(Getline[7]);
+					inner.add(Getline[8]);
+					inner.add(Getline[9]);
+					inner.add(Getline[10]);
+					inputCSV.add(inner);
+				}
+			}
+			/**
+			 * here i used bubblesort on the list and sorted is by signal(SSID)
+			 * as requested in the matala
+			 */
+			bubbleSort(inputCSV);
+			//print to check what i got:
+			/*System.out.println(headlines);
+			for(int i=0; i<=9; i++)
+			{
+				System.out.println(inputCSV.get(i));
+			}*/
+
+			/**
+			 * here i write the first 10 spots of my list + headlines line to 
+			 * a csv file (csvfile name is "finaltest2") and output it to my computer
+			 */
+			FileWriter writer = new FileWriter("C:\\Users\\computer\\Desktop\\csv\\finaltest2.csv");
+			List<String> TopTen = new ArrayList<>();
+			for(int k=0; k<headlines.size(); k++)
+			{
+				TopTen.add(headlines.get(k));
+			}
+			String collectheadlines= TopTen.stream().collect(Collectors.joining(","));
+			String collect0 = inputCSV.get(0).stream().collect(Collectors.joining(","));
+			String collect1 = inputCSV.get(1).stream().collect(Collectors.joining(","));
+			String collect2 = inputCSV.get(2).stream().collect(Collectors.joining(","));
+			String collect3 = inputCSV.get(3).stream().collect(Collectors.joining(","));
+			String collect4 = inputCSV.get(4).stream().collect(Collectors.joining(","));
+			String collect5 = inputCSV.get(5).stream().collect(Collectors.joining(","));
+			String collect6 = inputCSV.get(6).stream().collect(Collectors.joining(","));
+			String collect7 = inputCSV.get(7).stream().collect(Collectors.joining(","));
+			String collect8 = inputCSV.get(8).stream().collect(Collectors.joining(","));
+			String collect9 = inputCSV.get(9).stream().collect(Collectors.joining(","));
+
+			writer.write(collectheadlines);
+			writer.write("\n");
+			writer.write(collect0);
+			writer.write("\n");
+			writer.write(collect1);
+			writer.write("\n");
+			writer.write(collect2);
+			writer.write("\n");
+			writer.write(collect3);
+			writer.write("\n");
+			writer.write(collect4);
+			writer.write("\n");
+			writer.write(collect5);
+			writer.write("\n");
+			writer.write(collect6);
+			writer.write("\n");
+			writer.write(collect7);
+			writer.write("\n");
+			writer.write(collect8);
+			writer.write("\n");
+			writer.write(collect9);
+			writer.close();
+
+
+
+
+
+// this is to catch Exception and wrong files
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+	/**
+	 * this is the bubble sort i made to sort my list by signals(SSID)
+	 * @param arr
+	 */
+	static void bubbleSort(List<ArrayList<String>> arr)
+	{  
+		int n = arr.size();  
+		ArrayList<String> temp=new ArrayList<String>();  
+		for(int i=0; i < n; i++){  
+			for(int j=1; j < (n-i); j++){ 
+				int n1=Integer.parseInt(arr.get(j-1).get(5));
+				int n2=Integer.parseInt(arr.get(j).get(5));
+				if(n1 < n2){  
+					//swap elements  
+					temp = arr.get(j-1); 
+					arr.set(j-1, arr.get(j));  
+					arr.set(j, temp);
+				}  
+
+			}  
+		}  
+	}
+	
+}
---
+++ WriteToKML.java
@@ -0,0 +1,146 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import javax.swing.plaf.synth.SynthSpinnerUI;
+
+
+import java.util.ArrayList;
+import java.io.FileWriter;
+
+public class WriteToKML {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/**
+		 * here i again input a csv file (the one with the top 10 signals)
+		 * and insert the data to a collection (arraylist of arraylists)
+		 * 
+		 * please note that you need to change the directory location according to
+		 * your computer for it to read and export the file successfully
+		 */
+		String csvFile = "C:\\Users\\computer\\Desktop\\csv\\finaltest2.csv";
+		BufferedReader br = null;
+		String line = "";
+		String cvsSplitBy = ",";
+
+		try {
+
+			br = new BufferedReader(new FileReader(csvFile));
+			List<ArrayList<String>> mycsv=new ArrayList<ArrayList<String>>();
+			while ((line = br.readLine()) != null) {
+
+				// use comma as separator
+				String[] input = line.split(cvsSplitBy);
+				ArrayList<String> inner=new ArrayList<String>();
+				inner.add(input[0]);
+				inner.add(input[1]);
+				inner.add(input[2]);
+				inner.add(input[3]);
+				inner.add(input[4]);
+				inner.add(input[5]);
+				inner.add(input[6]);
+				inner.add(input[7]);
+				inner.add(input[8]);
+				inner.add(input[9]);
+				inner.add(input[10]);
+				mycsv.add(inner);
+			}
+			//print to check what i got
+			/*for(int i=0; i<=9; i++)
+			{
+				System.out.println(mycsv.get(i));
+			}*/
+			
+			/**
+			 * here i filter my list by Time, ID or location
+			 * and insert the filtered list to a new list.
+			 * in this example i chose to filter by ID and Time.
+			 * when you check our assignment you can change it and filter whatever you like :)
+			 */
+			Predicate<ArrayList<String>> condition1=s -> s.get(1).contains("Partner") & s.get(3).contains("20:11");
+			//this is example of filtering by ID and time;
+			List<ArrayList<String>> filteredStrings=filterby(mycsv ,condition1);
+			//print to check what i got
+			/*for(int j=0; j<filteredStrings.size(); j++)
+			{
+				System.out.println(filteredStrings.get(j));
+			}*/
+
+			
+			/**
+			 * here i am writing my filtered list into a kml file 
+			 * and then output it to my computer 
+			 */
+			FileWriter writer2 = new FileWriter("C:\\Users\\computer\\Desktop\\csv\\KMLoutput.kml");
+			writer2.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>   ");
+			writer2.write("<kml xmlns=\"http://earth.google.com/kml/2.0\">   ");
+			writer2.write("<Document>   ");
+			for(int k=0; k<filteredStrings.size(); k++)
+			{
+				writer2.write("<Placemark>");
+				writer2.write("<name>"+filteredStrings.get(k).get(1)+"</name>");
+				writer2.write("<description>"+filteredStrings.get(k).get(10)+"</description>");
+				writer2.write("<Point><coordinates>"+filteredStrings.get(k).get(7)+","+filteredStrings.get(k).get(6)+","+filteredStrings.get(k).get(8)+"</coordinates></Point>");
+				writer2.write("</Placemark>");
+			}
+			writer2.write("</Document>");
+			writer2.write("</kml>");
+			writer2.close();
+
+
+
+
+/**
+ * again to catch Exception and wrong files
+ */
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+	/**
+	 * this function help me to filter the list by time,location or ID
+	 * @param strings
+	 * @param condition
+	 * @return
+	 */
+	public static List<ArrayList<String>> filterby(List<ArrayList<String>> strings, Predicate<ArrayList<String>> condition)
+	{
+		List<ArrayList<String>> output=new ArrayList<ArrayList<String>>(); //initalize empty list
+		for(int i=0; i<strings.size(); i++)
+		{
+			if(condition.test(strings.get(i))==true)
+			{
+				output.add(strings.get(i));
+			}
+		}
+		return output;
+
+	}
+
+
+
+
+}
+
+
