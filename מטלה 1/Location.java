8fa111cf9eac6678e73e8c6b05644f3e5f3501b7
levioni <33033952+levioni@users.noreply.github.com>
2017-11-20 23:13:11 +0200

    Add files via upload
    matala 0 fixed with classes and methods
    matala 1 need to finish Junits testing

---
+++ מטלה 1/Location.java
@@ -0,0 +1,83 @@
+
+
+/**
+ * this class define Location by Lat,Lon,Alt,ID and time
+ * @author computer
+ *
+ */
+public class Location {
+	
+	public double Lat;
+	public double Lon;
+	public double Alt;
+	public String ID; 
+	public String Time;
+	
+	public Location(double Lat, double Lon, double Alt,String ID, String time)
+	{
+		this.Lat=Lat;
+		this.Lon=Lon;
+		this.Alt=Alt;
+		this.ID=ID;
+		this.Time=time;
+	}
+	public boolean equalocation(Location L)
+	{
+		if(this.Lat==L.Lat & this.Lon==L.Lon & this.Alt==L.Alt)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	
+	
+	
+
+	public double getLat() {
+		return Lat;
+	}
+	public void setLat(double lat) {
+		Lat = lat;
+	}
+	public double getLon() {
+		return Lon;
+	}
+	public void setLon(double lon) {
+		Lon = lon;
+	}
+	public double getAlt() {
+		return Alt;
+	}
+	public void setAlt(double alt) {
+		Alt = alt;
+	}
+	public String getID() {
+		return ID;
+	}
+	public void setID(String iD) {
+		ID = iD;
+	}
+	public String getTime() {
+		return Time;
+	}
+	public void setTime(String time) {
+		Time = time;
+	}
+	@Override
+	public String toString() {
+		return "" + Lat + "," + Lon + "," + Alt + "," + ID + "," + Time + "";
+	}
+
+
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ מטלה 1/filter.java
@@ -0,0 +1,11 @@
+ 
+public interface filter {
+	/**
+	 * this is the interface i used to filter my list
+	 * @param s
+	 * @return
+	 */
+	
+	public boolean test(String s);
+
+}
